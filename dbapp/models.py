3c2b25ab4848f2ca7f3fc26be60ce3cb658f7e83
gurram444 <gurramnarendra5@gmail.com>
2019-03-02 15:01:36 +0530

    Initial commit

---
+++ dbapp/models.py
@@ -0,0 +1,9 @@
+# Create your models here.
+from django.db import models
+class Product(models.Model):
+    pid=models.IntegerField(primary_key=True)
+    pname=models.CharField(max_length=20)
+    pcost=models.DecimalField(max_digits=10,
+                              decimal_places=4)
+    pmfd=models.DateField()
+    pexpdt=models.DateField()
